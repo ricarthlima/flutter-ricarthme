d61c994531edaa6eb77dcbd81b7a23cb47d10e8c
Ricarth Lima <rrsl@cin.ufpe.br>
2021-01-20 01:53:45 -0300

    working screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,11 +9,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ricarth.me',
       debugShowCheckedModeBanner: false,
-      routes: {
-        '/': (context) => MyHomePage(),
-      },
+      theme: ThemeData(fontFamily: "NotoSans"),
+      home: AnimatedSplashScreen(
+        nextScreen: MyHomePage(),
+        splash: Image.asset("assets/ricarth.png"),
+        duration: 10,
+        backgroundColor: Colors.blue,
+      ),
     );
   }
 }
@@ -26,18 +31,45 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: SizedBox(
-          width: 400,
-          child: Container(
-            child: Text(
-              "Ricarth Lima",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 50,
+      body: Container(
+        color: Color.fromARGB(255, 255, 195, 0),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 50),
+                    ),
+                    Image.asset("assets/construction.gif"),
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 15),
+                    ),
+                    Text(
+                      "Ainda estamos trabalhando,\nvolte mais tarde.",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
               ),
-            ),
+              Container(
+                color: Colors.blue,
+                child: Text(
+                  "Feito por @ricarthlima com #Flutter",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontFamily: "NotoSans"),
+                ),
+              ),
+            ],
           ),
         ),
       ),
