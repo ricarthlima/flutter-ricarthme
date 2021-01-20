ff458769f1baa5c077244307bb6b02bfa4856dab
Ricarth Lima <rrsl@cin.ufpe.br>
2021-01-19 21:27:15 -0300

    add build folder

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       routes: {
         '/': (context) => MyHomePage(),
       },
