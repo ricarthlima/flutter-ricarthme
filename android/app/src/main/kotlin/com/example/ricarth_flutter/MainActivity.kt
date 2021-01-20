ea82ebd13e31f6f9d204689ee7efa152b1f163fb
Ricarth Lima <rrsl@cin.ufpe.br>
2021-01-19 21:19:25 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/ricarth_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ricarth_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      routes: {
+        '/': (context) => MyHomePage(),
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: SizedBox(
+          width: 400,
+          child: Container(
+            child: Text(
+              "Ricarth Lima",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 50,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
